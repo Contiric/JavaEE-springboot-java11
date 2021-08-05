f9556067f7c4484dd8c29114a2df5355c2f4e3a6
Ricardo Conti <ricardoconti2718@gmail.com>
2021-08-05 11:53:49 -0300

    Entidade User e seu resource

---
+++ src/main/java/com/ricardoconti/JavaEEcourse/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.ricardoconti.JavaEEcourse.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ricardoconti.JavaEEcourse.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		
+		User u = new User(1L, "maria", "maria@gmail.com", "9999999999", "123456");
+		
+		return ResponseEntity.ok().body(u);
+	}
+
+}
