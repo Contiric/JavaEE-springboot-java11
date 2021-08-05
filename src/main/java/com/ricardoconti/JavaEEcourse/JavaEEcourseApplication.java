7841c5ec861ce75c0a201d51c4704f1e5f2bc40f
Ricardo Conti <ricardoconti2718@gmail.com>
2021-08-05 11:13:48 -0300

    Projeto Criado

---
+++ src/main/java/com/ricardoconti/JavaEEcourse/JavaEEcourseApplication.java
@@ -0,0 +1,13 @@
+package com.ricardoconti.JavaEEcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEEcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEEcourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardoconti/JavaEEcourse/JavaEEcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardoconti.JavaEEcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEEcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
