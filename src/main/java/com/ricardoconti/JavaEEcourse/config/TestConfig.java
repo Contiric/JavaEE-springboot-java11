062d24054baf54834737bbec74fc021d0ad92e34
Ricardo Conti <ricardoconti2718@gmail.com>
2021-08-06 16:16:52 -0300

    Entidade Product

--- src/main/java/com/ricardoconti/JavaEEcourse/config/TestConfig.java
+++ src/main/java/com/ricardoconti/JavaEEcourse/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.ricardoconti.JavaEEcourse.entities.Category;
 import com.ricardoconti.JavaEEcourse.entities.Order;
+import com.ricardoconti.JavaEEcourse.entities.Product;
 import com.ricardoconti.JavaEEcourse.entities.User;
 import com.ricardoconti.JavaEEcourse.entities.enums.OrderStatus;
 import com.ricardoconti.JavaEEcourse.repositories.CategoryRepository;
 import com.ricardoconti.JavaEEcourse.repositories.OrderRepository;
+import com.ricardoconti.JavaEEcourse.repositories.ProductRepository;
 import com.ricardoconti.JavaEEcourse.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -44,6 +49,15 @@ public class TestConfig implements CommandLineRunner{
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
+		
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
--- src/main/java/com/ricardoconti/JavaEEcourse/entities/Product.java
+++ src/main/java/com/ricardoconti/JavaEEcourse/entities/Product.java
@@ -12,27 +12,34 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
+
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String name;
-	
-	@Transient
-	private Set<Category> products = new HashSet<>();
-	
-	public Category() {
-		
-	}
+	 private Long id;
+	 private String name;
+	 private String description;
+	 private Double price;
+	 private String imgUrl;
+	 
+	 @Transient
+	 private Set<Category> categories = new HashSet<>();
+	 
+	 public Product() {
+		 
+	 }
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -50,14 +57,34 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
 
-	public Set<Category> getProducts() {
-		return products;
+	public String getDescription() {
+		return description;
 	}
-	
-	
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
 
 	@Override
 	public int hashCode() {
@@ -72,11 +99,12 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
 
 	
 	
-	
+	 
+
 }
