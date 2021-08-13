fe917ab546794e2a69064caeb3a63da6d3f3260a
Ricardo Conti <ricardoconti2718@gmail.com>
2021-08-13 17:19:06 -0300

    Exception handling -delete

--- src/main/java/com/ricardoconti/JavaEEcourse/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/ricardoconti/JavaEEcourse/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.ricardoconti.JavaEEcourse.services.exceptions.DataBaseException;
 import com.ricardoconti.JavaEEcourse.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -23,6 +24,15 @@ public class ResourceExceptionHandler {
 		
 	}
 	
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request) {
+		String error = "Data base error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		
+	}
 	
 	
 	
--- src/main/java/com/ricardoconti/JavaEEcourse/services/UserService.java
+++ src/main/java/com/ricardoconti/JavaEEcourse/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.ricardoconti.JavaEEcourse.entities.User;
 import com.ricardoconti.JavaEEcourse.repositories.UserRepository;
+import com.ricardoconti.JavaEEcourse.services.exceptions.DataBaseException;
 import com.ricardoconti.JavaEEcourse.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -28,11 +31,17 @@ public class UserService {
 	public User insert (User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update (Long id, User obj) {
 		User entity = repository.getOne(id);
 		updateData(entity, obj);
